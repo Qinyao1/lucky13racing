41d588d5ec52326d81371b22be65c003597e43f7
castroja <castroveritas@gmail.com>
2018-03-05 16:22:15 -0500

    Anchored camera to vehicle

--- game/podracer.js
+++ game/podracer.js
@@ -1,20 +1,21 @@
-	// Here we define some controls to interact with the animation ...
-	var controls = new function() {
-			this.camera = 0;
-	}
-
-	var gui = new dat.GUI();
-	gui.add(controls, 'camera', [0,1,2,3]);
-	console.dir([controls, controls.rotationX, controls.rotationZ]);
-
 	// First we declare the variables that hold the objects we need
 	// in the animation code
 	var scene, camera, renderer;  // all threejs programs need these
 	var planeMesh, podRacer, testTrack;
 	var light1,light2;  // we have two lights
+	var avatar, avatarCam;
+
+	var controls =
+	     {fwd:false, bwd:false, left:false, right:false,
+				speed:10,
+		    camera:camera}
+
+	var gameState =
+				     {score:0, health:10, scene:'main', camera:'none' }
 
 	init(); // initialize these 9 variables
 	animate();  // start the animation loop!
+
 
 
 
@@ -24,23 +25,29 @@
 	function init(){
 			initScene();
 			initPlaneMesh();
-			initPodRacer();
 			initTestTrack();
-
+			initControls();
 			initLight1();
 			initLight2();
 			initCamera();
+			initPodRacer();
 	}
+
+	function initPhysijs(){
+	    Physijs.scripts.worker = '../js/physijs_worker.js';
+	    Physijs.scripts.ammo = '../js/ammo.js';
+	  }
 
 	/* Initializes Scene and Renderer
 	*/
 	function initScene(){
-		scene = new THREE.Scene();
+		scene = new Physijs.Scene();
 		renderer = new THREE.WebGLRenderer();
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		document.body.appendChild( renderer.domElement );
 		renderer.shadowMap.enabled = true;
 		renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+		initPhysijs();
 	}
 
 	/*
@@ -48,8 +55,8 @@
 	*/
 	function initCamera(){
 		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-		camera.position.z = 20;
-		camera.position.y=4;
+		camera.position.set(0,4,0);
+		camera.lookAt(0,4,10);
 	}
 
 	function initPodRacer(){
@@ -57,9 +64,15 @@
 		var texture = new THREE.TextureLoader().load( '../textures/b2.png' );
 		loader.load("../models/podracer.json",
 					function ( geometry, materials ) {
-						var material = new THREE.MeshPhongMaterial({color:0xffffff, map:texture});
-						podRacer = new THREE.Mesh( geometry, material );
-						scene.add( podRacer )});
+						material = new THREE.MeshPhongMaterial({color:0xffffff, map:texture});
+						pmaterial = new Physijs.createMaterial(material,0.9,0.5);
+						podRacer = new Physijs.BoxMesh( geometry, pmaterial, 0 );
+						avatar = podRacer;
+						camera.position.set(0,6,-13);
+						camera.lookAt(0,4,10);
+						podRacer.add(camera);
+						scene.add(podRacer);
+						});
 	}
 
 	function initTestTrack(){
@@ -67,7 +80,8 @@
 		loader.load("../models/FullTestTrack.json",
 					function ( geometry, materials ) {
 						var material = new THREE.MeshPhongMaterial({color:0xffff00});
-						testTrack = new THREE.Mesh( geometry, material );
+						var pmaterial = new Physijs.createMaterial(material,0.9,0.5);
+						testTrack = new Physijs.BoxMesh( geometry, material, 0);
 						testTrack.scale.set(50,50,50);
 						scene.add( testTrack )});
 	}
@@ -112,16 +126,97 @@
 	}
 
 
+	var clock;
+
+	function initControls(){
+			// here is where we create the eventListeners to respond to operations
+
+			  //create a clock for the time-based animation ...
+				clock = new THREE.Clock();
+				clock.start();
+
+				window.addEventListener( 'keydown', keydown);
+				window.addEventListener( 'keyup',   keyup );
+	  }
+
+		function keydown(event){
+			console.log("Keydown:"+event.key);
+			console.dir(event);
+			// first we handle the "play again" key in the "youwon" scene
+			// this is the regular scene
+			switch (event.key){
+				// change the way the avatar is moving
+				case "w": controls.fwd = true;  break;
+				case "s": controls.bwd = true; break;
+				case "a": controls.left = true; break;
+				case "d": controls.right = true; break;
+				case "r": controls.up = true; break;
+				case "f": controls.down = true; break;
+				case "m": controls.speed = 30; break;
+
+				// switch cameras
+				case "1": camera.position.set(0,4,-6);; break;
+				case "2": camera.position.set(0,4,-6);; break;
+
+				// move the camera around, relative to the avatar
+				case "ArrowLeft": camera.translateY(1);break;
+				case "ArrowRight": camera.translateY(-1);break;
+				case "ArrowUp": camera.translateZ(-1);break;
+				case "ArrowDown": camera.translateZ(1);break;
+
+			}
+
+		}
+
+		function keyup(event){
+			//console.log("Keydown:"+event.key);
+			//console.dir(event);
+			switch (event.key){
+				case "w": controls.fwd   = false;  break;
+				case "s": controls.bwd   = false; break;
+				case "a": controls.left  = false; break;
+				case "d": controls.right = false; break;
+				case "r": controls.up    = false; break;
+				case "f": controls.down  = false; break;
+				case "m": controls.speed = 10; break;
+			}
+		}
+
+
+
+	  function updateAvatar(){
+			"change the avatar's linear or angular velocity based on controls state (set by WSAD key presses)"
+
+			var forward = avatar.getWorldDirection();
+
+			if (controls.fwd){
+				podRacer.setLinearVelocity(forward.multiplyScalar(controls.speed));
+			} else if (controls.bwd){
+				podRacer.setLinearVelocity(forward.multiplyScalar(-controls.speed));
+			} else {
+				var velocity = avatar.getLinearVelocity();
+				velocity.x=velocity.z=0;
+				podRacer.setLinearVelocity(velocity); //stop the xz motion
+			}
+
+			if (controls.left){
+				podRacer.setAngularVelocity(new THREE.Vector3(0,controls.speed*0.1,0));
+			} else if (controls.right){
+				podRacer.setAngularVelocity(new THREE.Vector3(0,-controls.speed*0.1,0));
+			}
+
+		}
 
   var angle = 0;
 
 	function animate() {
 		requestAnimationFrame( animate );
-		var currentTime = (new Date()).getTime();
-		if(podRacer != null){
-			camera.lookAt(podRacer.position);
-		}
 
+						updateAvatar();
+			    	scene.simulate();
+						if (gameState.camera!= 'none'){
+							renderer.render( scene, gameState.camera );
+						}
 
 		angle += controls.camRotation;
 		switch (controls.camera){
@@ -132,39 +227,4 @@
 		}
 
 		renderer.render( scene, camera );
-	}
-
-
-
-	/* here we make the camera move in a circle of radius 20
-		around the scene looking always at the coneMesh object
-		We also have it oscillate higher and lower
-	*/
-	function revolveCamera(angle){
-		camera.position.x = 5*Math.sin(angle);
-		camera.position.z = 5*Math.cos(angle);
-		camera.position.y = 2*(1+Math.cos(angle));
-		camera.lookAt(0,0,0);
-	}
-
-
-	function setCameraAngle1(){
-		camera.position.x=0;
-		camera.position.y=1;
-		camera.position.z=10;
-		camera.lookAt(0,0,0);
-	}
-
-	function setCameraAngle2(){
-		camera.position.x=0;
-		camera.position.y=10;
-		camera.position.z=0;
-		camera.lookAt(0,0,0);
-	}
-
-	function setCameraAngle3(){
-		camera.position.x=0;
-		camera.position.y=4;
-		camera.position.z=30;
-		camera.lookAt(0,4,0);
 	}
