5a6e55c46161fe606e42f14b973adfb4b780dc5b
castroja <castroveritas@gmail.com>
2018-04-09 21:07:08 -0400

    Changed game theme

--- game/podracer.js
+++ game/podracer.js
@@ -6,7 +6,6 @@
 	var clock;
 	var listener, sound, audioLoader;
 	var startScene, startCamera, startText;
-	var deathScene, deathCamera, deathText;
 	var controls =
 	     {  fwd:false, bwd:false, left:false, right:false,
 					hardLeft:false, hardRight:false, boost: false,
@@ -47,11 +46,11 @@
 		scene = new Physijs.Scene();
 
 		//Initializes skybox
-		var geometry = new THREE.SphereGeometry( 1000, 1000, 80 );
-		var texture = new THREE.TextureLoader().load( '/textures/space.PNG' );
+		var geometry = new THREE.SphereGeometry( 550, 550, 80 );
+		var texture = new THREE.TextureLoader().load( '/textures/clouds.png' );
 		texture.wrapS = THREE.RepeatWrapping;
 		texture.wrapT = THREE.RepeatWrapping;
-		texture.repeat.set( 10, 10 );
+		texture.repeat.set( 7, 1 );
 		var material = new THREE.MeshLambertMaterial( { color: 0xffffff, map:texture, side:THREE.DoubleSide } );
 		var mesh = new THREE.Mesh( geometry, material, 0 );
 		scene.add( mesh );
@@ -99,14 +98,14 @@
 	function initPodRacer(){
 		console.log("hello!");
 		var loader = new THREE.JSONLoader();
-		var texture = new THREE.TextureLoader().load( '../textures/podRacer.png' );
-		loader.load("../models/racer.json",
+		var texture = new THREE.TextureLoader().load( '../textures/diffuse.bmp' );
+		loader.load("../models/feisar.json",
 					function ( geometry, materials ) {
 						material = new THREE.MeshLambertMaterial({color:0xffffff, map:texture});
 						pmaterial = new Physijs.createMaterial(material,0.1,0.5);
 						podRacer = new Physijs.BoxMesh(geometry, pmaterial, 1000);
 						podRacer.add(camera);
-						podRacer.position.x = 500;
+						podRacer.position.x = 350;
 						scene.add(podRacer);
 						scene.add(testTrack);
 						animate();
@@ -134,6 +133,20 @@
 		Physijs.scripts.ammo = '/js/ammo.js';
 	}
 
+	// function initTrack(){
+	// 	var loader = new THREE.JSONLoader();
+	// 	var texture = new THREE.TextureLoader().load( '../textures/desert.png' );
+	// 	loader.load("../models/racetrack.json",
+	// 				function ( geometry, materials ) {
+	// 					material = new THREE.MeshLambertMaterial({color:0xffffff, map:texture, side:THREE.DoubleSide });
+	// 					pmaterial = new Physijs.createMaterial(material,0.1,0.5);
+	// 					testTrack = new Physijs.BoxMesh(geometry, pmaterial, 0);
+	// 					testTrack.position.set(-1,-1,-1);
+	// 					testTrack.rotation.x = Math.PI;
+	// 					testTrack.scale.set(100,100,100);
+	// 					});
+	// }
+
 	function initTrack() {
 		var geometry = new THREE.RingGeometry();
 		var texture = new THREE.TextureLoader().load( '../textures/desert.png' );
@@ -146,25 +159,6 @@
 		scene.add(testTrack);
 	}
 
-	// function createDeathScene() {
-	// 	deathScene = new Physijs.Scene();
-	// 	var geometry = new THREE.SphereGeometry( 1000, 1000, 80 );
-	// 	var texture = new THREE.TextureLoader().load( '../textures/youdied.png' );
-	// 	texture.wrapS = THREE.RepeatWrapping;
-	// 	texture.wrapT = THREE.RepeatWrapping;
-	// 	texture.repeat.set( 10, 10 );
-	// 	var material = new THREE.MeshLambertMaterial( { color: 0xffffff, map:texture, side:THREE.DoubleSide } );
-	// 	var deathText = new THREE.Mesh( geometry, material, 0 );
-	// 	deathScene.add(deathText);
-	// 	var light1 = new THREE.PointLight( 0xffffff);
-	// 	light1.position.set(0,200,20);
-	// 	deathScene.add(light1);
-	// 	deathCamera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
-	// 	deathCamera.position.set(0,50,1);
-	// 	deathCamera.lookAt(0,0,0);
-	// 	initGame();
-	// }
-	
 	function initControls(){
 				clock = new THREE.Clock();
 				clock.start();
@@ -304,14 +298,6 @@
 				podRacer.setAngularVelocity(new THREE.Vector3(0,0,0));
 			}
 
-			// Falling of test track
-			if ((podRacer.position.x > 500 || podRacer.position.x < -500) || (podRacer.position.x < 250 && podRacer.position.x > - 250)) {
-				podRacer.position.y = -1000
-				// createDeathScene();
-			}
-			if ((podRacer.position.z > 500 || podRacer.position.z < -500) || (podRacer.position.z < 250 && podRacer.position.z > - 250)) {
-				podRacer.position.y = -1000
-			}
 		}
 
 	function animate() {
